2d8699f98403347dc6875a39b69a0b8122e2ead2
Mridul Barman <mridulbarman027@gmail.com>
2024-04-13 14:26:54 +0530

    added model

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,3 +7,56 @@ const agentSchema = new Schema({
 });
 
 export const AgentModel = model(COLLECTION.AGENT, agentSchema);
+
+const userSchema = new Schema({
+  firstname: String,
+  dob: Date,
+  address: String,
+  phone: String,
+  state: String,
+  zip: String,
+  email: String,
+  gender: String,
+  userType: String,
+});
+
+export const UserModel = model(COLLECTION.USER, userSchema);
+
+const policyCarrierSchema = new Schema({
+  company_name: String,
+});
+
+export const PolicyCarrierModel = model(COLLECTION.POLICY_CARRIER, policyCarrierSchema);
+
+const policyCategorySchema = new Schema({
+  category_name: String,
+});
+
+export const PolicyCategoryModel = model(COLLECTION.POLICY_CATEGORY, policyCategorySchema);
+
+const userAccountSchema = new Schema({
+  account_name: String,
+  user_id: String,
+});
+
+export const UserAccountModel = model(COLLECTION.USER_ACCOUNT, userAccountSchema);
+
+const policyInfoSchema = new Schema({
+  policy_number: String,
+  policy_start_date: String,
+  policy_end_date: String,
+  policy_carrier_id: String,
+  policy_category_id: String,
+  agent_id: String,
+  user_id: String,
+  user_account_id: String,
+});
+
+export const PolicyInfoModel = model(COLLECTION.POLICY_INFO, policyInfoSchema);
+
+const messageSchema = new Schema({
+  message: String,
+  date_time: Date,
+});
+
+export const MessageModel = model(COLLECTION.USER_ACCOUNT, messageSchema);
